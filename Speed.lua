a9bf1fc00c5863b82f4de481332346cdaa033ade
Xenijo <80117820+Xenijo@users.noreply.github.com>
2023-01-15 09:24:52 +0100

    Update Speed.lua

--- Speed.lua
+++ Speed.lua
@@ -1,2 +1,3 @@
- game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector * 5
+getgenv().version = 1.0 
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.lookVector * 5
         
